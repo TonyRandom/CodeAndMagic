ff5bef5ef614aa294bb638cf563b27e957102e27
TonyRandom <65846634+TonyRandom@users.noreply.github.com>
2020-09-29 10:51:12 +0300

    home task

--- js/setup.js
+++ js/setup.js
@@ -2,11 +2,12 @@ var setup = document.querySelector('.setup');
 setup.classList.remove('hidden');
 
 
+//МАССИВЫ СО ЗНАЧЕНИЯМИ
+
 var NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
 var SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
 var COAT_COLOR = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var EYES_COLOR = ['black', 'red', 'blue', 'yellow', 'green'];
-var  FIREBALL_COLOR = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
 
 //функция, выдющая случайное значение из массива
 var randomizeFromArray = function (arrayName) {
@@ -16,17 +17,16 @@ var randomizeFromArray = function (arrayName) {
 
 // генерируем каждый ключ по-отдельности
 
-
     var name = randomizeFromArray(NAMES) + ' ' + randomizeFromArray(SURNAMES);
     
     var coatColor = randomizeFromArray(COAT_COLOR);
 
     var eyesColor = randomizeFromArray(EYES_COLOR);
 
-    var fireballColor = randomizeFromArray(FIREBALL_COLOR);
+    
 
 
-    //создаем одного волшебника
+    //создаем одного волшебника ФУНКЦИЯ
 
 
     var generateWizard = function () {
@@ -38,8 +38,6 @@ var randomizeFromArray = function (arrayName) {
         
             eyesColor: randomizeFromArray(EYES_COLOR),
         
-            fireballColor: randomizeFromArray(FIREBALL_COLOR),
-        
             }
 
        
@@ -48,10 +46,9 @@ var randomizeFromArray = function (arrayName) {
 
     // по заданию надо сгенерировать массив из 4 рандомных волшебников
 
-    // создаю функцию создания волшебника, которую буду вызывать в цикле
+   
 
-
-    var wizardsData = [];
+    var wizardsData = []; // МАССИВ С ВОЛШЕБНИКАМИ
 
     for (var i = 0; i < 4; i++) {
         generateWizard();
@@ -68,27 +65,74 @@ var randomizeFromArray = function (arrayName) {
 
     //НЕ ПОНИМАЮ, КАК СВЯЗАТЬ ПРЕДЫДУЩЕЕ ЗАДАНИЕ С ТЕКУЩИМ
 
+
+    // ШАБЛОН ВОЛШЕБНИКА. В НЕГО НАДО ДОБАВИТЬ ВСЕ, А ПОТОМ ВСТАВЛЯТЬ В КОНТЕЙНЕР
     var wizardList = document.querySelector('#similar-wizard-template');
-    console.log(wizardList);
+
+
+    //  СОЗДАЕМ ВЕРСТКУ ОДНОГО ВОЛШЕБНИКА
 
     var setupSimilar = document.createElement('div');
     setupSimilar.classList.add('.setup-similar-label');
     setupSimilar.textContent = name;
-    console.log(setupSimilar.textContent);
-
+    
     var coat = document.querySelector('.wizard-coat');
     coat.style.fill = coatColor;
-    console.log(coat);
+   
     
     var eyes = document.querySelector('.wizard-eyes');
     eyes.style.fill = eyesColor;
-    console.log(eyes);
+  
     
+    // ФУНКЦИЯ ДОБАВЛЕНИЯ ВСЕХ СВОЙСТВ В РАЗМЕТКУ.
+
+    var createWizard = function () {
+
+
+    }
+
+
+    /* 	Иногда бывает удобно сгруппировать однотипные или разнотипные элементы и вставить их все вместе. Тогда для этого можно использовать специальный элемент documentFragment.
+
+    var wizardTemplate = document.createDocumentFragment();
+    
+    for (var i = 0; i < 6; i++) {
+        var newElement = document.createElement('el');
+        newElement.className = 'el';
+
+        fragment.appendChild(newElement);
+    }
+
+        wizardContainer.appendChild(wizardTemplate);
+
+    */
 
     
     //КУДА ВСТАВЛЯТЬ ВОЛШЕБНИКОВ
     var wizardContainer = document.querySelector('.setup-similar-list');
-    console.log(wizardContainer);
+    console.log(wizardContainer.children);
+
+
+
+
+    //ФУНКЦИЯ ВСТАВКИ ВОЛШЕБНИКОВ - НЕВЕРНО!!!!!!!!!!!!!!!!!
+/*     var renderCards = function(products) { 
+  
+    for(var i = 0; i< products.length; i ++) {
+          
+        var wizardCard = products[i];
+          
+       // var wizardItem=createCard(wizardCard); /////////////////////////////////////////////////
+          
+        wizardContainer.appendChild(wizardCard);
+        }
+        
+        }
+       */
+
+
+    //вызов функции с массивом товаров 
+    /* renderCards(wizardsData); */
 
 
     var similarWizardsBlock = document.querySelector('.setup-similar');
